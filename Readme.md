e156477d8f9083f0333d1ba311d67b3bf759e072
Sankalpa <SABEYGUNA@csr.com.au>
2024-04-26 22:49:07 +0530

    Basic working map

--- Readme.md
+++ Readme.md
@@ -1,3 +1,11 @@
 Get started with next.js 
 
+npm Dependencies 
+1. `npm i "@vis.gl/react-google-maps"`
+
+
+environment varialbles
+1. `GOOGLE_MAPS_API_KEY=`
+  Use to hold google map api key.
+
 
--- pages/components/MapComponent.tsx
+++ pages/components/MapComponent.tsx
@@ -1,7 +1,25 @@
+"use client";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { useMemo } from "react";
+
+type LatLngLiteral = google.maps.LatLngLiteral;
+
 export default function MapComponent() {
-    return (
-        <div>
-            Map component will render here!
-        </div>
-    )
-};
+  const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY;
+  const mapCenter = useMemo<LatLngLiteral>(
+    () => ({ lat: 7.9840886, lng: 80.7029496 }),
+    []
+  );
+
+  if (googleMapsApiKey == undefined) {
+    return <div>Map loading......;</div>;
+  }
+
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <APIProvider apiKey={googleMapsApiKey}>
+        <Map center={mapCenter} zoom={9}></Map>
+      </APIProvider>
+    </div>
+  );
+}
