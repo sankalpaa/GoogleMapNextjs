ce53db6419df9cf2628da97300933597e9891961
Sankalpa <SABEYGUNA@csr.com.au>
2024-05-03 21:54:08 +0530

    Add next.config.js file to access env in client side

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    reactStrictMode: true,
+    env: {
+        GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY, //to access the env in client side
+    }
+  }
--- pages/components/MapComponent.tsx
+++ pages/components/MapComponent.tsx
@@ -1,21 +1,27 @@
 "use client";
+
 import { APIProvider, Map } from "@vis.gl/react-google-maps";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 type LatLngLiteral = google.maps.LatLngLiteral;
 
 export default function MapComponent() {
   const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY;
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   const mapCenter = useMemo<LatLngLiteral>(
     () => ({ lat: 7.9840886, lng: 80.7029496 }),
     []
   );
 
-  if (googleMapsApiKey == undefined) {
-    return <div>Map loading......;</div>;
+  if (googleMapsApiKey == undefined && isClient) {
+    return <div>Map loading.</div>;
   }
 
-  return (
+  return (isClient && googleMapsApiKey && 
     <div style={{ height: "100vh", width: "100%" }}>
       <APIProvider apiKey={googleMapsApiKey}>
         <Map center={mapCenter} zoom={9}></Map>
