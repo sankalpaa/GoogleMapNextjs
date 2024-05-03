c0ab581ff29570610eb46f447a0d6d040d9d44c0
Sankalpa <SABEYGUNA@csr.com.au>
2024-05-03 22:12:21 +0530

    Show basic marker with static geo location

--- next.config.js
+++ next.config.js
@@ -2,5 +2,6 @@ module.exports = {
     reactStrictMode: true,
     env: {
         GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY, //to access the env in client side
+        GOOGLE_MAPS_ID: process.env.GOOGLE_MAPS_ID, 
     }
   }
--- pages/components/MapComponent.tsx
+++ pages/components/MapComponent.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { APIProvider, AdvancedMarker, Map } from "@vis.gl/react-google-maps";
 import { useEffect, useMemo, useState } from "react";
 
 type LatLngLiteral = google.maps.LatLngLiteral;
 
 export default function MapComponent() {
   const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY;
+  const googleMapId = process.env.GOOGLE_MAPS_ID;
   const [isClient, setIsClient] = useState(false);
   useEffect(() => {
     setIsClient(true);
@@ -21,11 +22,19 @@ export default function MapComponent() {
     return <div>Map loading.</div>;
   }
 
-  return (isClient && googleMapsApiKey && 
-    <div style={{ height: "100vh", width: "100%" }}>
-      <APIProvider apiKey={googleMapsApiKey}>
-        <Map center={mapCenter} zoom={9}></Map>
-      </APIProvider>
-    </div>
+  return (
+    isClient &&
+    googleMapsApiKey &&
+    googleMapId && (
+      <div style={{ height: "100vh", width: "100%" }}>
+        <APIProvider apiKey={googleMapsApiKey}>
+          <Map center={mapCenter} zoom={9} mapId={googleMapId}>
+            <AdvancedMarker
+              position={{ lat: 7.9840886, lng: 80.7029496 }}
+            ></AdvancedMarker>
+          </Map>
+        </APIProvider>
+      </div>
+    )
   );
 }
